c3cc8aa37a18d0c46e2f9dae18ca78e5460e7a90
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-03 17:50:56 +0800

    学习 map映射

--- chapter3-1-1/05/add.go
+++ chapter3-1-1/05/add.go
@@ -2,21 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
-	add1()
-	add2()
-	//add1()
-}
+var myGreeting = map[string]string{"Tim": "T!", "Jenny": "J!"}
 
-var myGreeting = map[string]string{"Tim": "A!", "Jenny": "B!"}
+func main() {
+	fmt.Println(myGreeting)
+	//add1()
+	//add2()
+	//update()
+	//delete1()
+	delete2()
+}
 
 // 添加操作
 func add1() {
-	myGreeting["Harleen"] = "C!"
+	myGreeting["Harleen"] = "H!"
 	fmt.Println(myGreeting)
 }
 
 // 获取map长度
 func add2() {
 	fmt.Println(len(myGreeting))
+}
+
+// 修改操作
+func update() {
+	myGreeting["Jenny"] = "F!"
+	fmt.Println(myGreeting)
+}
+
+// 删除操作
+func delete1() {
+	delete(myGreeting, "Tim")
+	fmt.Println(myGreeting)
+}
+
+// 删除操作： 对不存在的key进行删除操作 不会报错
+func delete2() {
+	delete(myGreeting, "goat")
+	fmt.Println(myGreeting)
 }
